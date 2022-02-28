afe89f4723bb83b3133bbff00b3e61d5febb522e
izuru775 <izuru775@gmail.com>
2022-02-28 14:09:42 +1100

    Progressing...

--- src/main.rs
+++ src/main.rs
@@ -10,28 +10,25 @@
     Methods : snake_case
 */
 
-struct Rectangle{
-    widht:f64,
-    height:f64
+struct Rectangle {
+    widht: f64,
+    height: f64,
 }
-impl Rectangle{
-    fn get_area(&self)->f64{
+impl Rectangle {
+    fn get_area(&self) -> f64 {
         self.widht * self.height
     }
 
-    fn scale(&mut self,scale:f64){
-        self.widht =  self.widht *scale;
-        self.height =  self.height *scale;
+    fn scale(&mut self, scale: f64) {
+        self.widht = self.widht * scale;
+        self.height = self.height * scale;
     }
-    fn new(widht:&f64, height:&f64) -> Rectangle{
-        
+    fn new(widht: &f64, height: &f64) -> Rectangle {
+        Rectangle {
+            widht: f64,
+            height: f64,
+        }
     }
 }
 
-
-
-fn main() {
-
- 
-
-}
+fn main() {}
