724c6afd1ad8f03b1623c4459ae05232fe68cd3a
izuru775 <izuru775@gmail.com>
2022-02-28 14:17:36 +1100

    Challenge completed

--- src/main.rs
+++ src/main.rs
@@ -20,15 +20,23 @@ impl Rectangle {
     }
 
     fn scale(&mut self, scale: f64) {
-        self.width = self.width * scale;
-        self.height = self.height * scale;
+        self.width *= scale;
+        self.height *= scale;
     }
     fn new(width: f64, height: f64) -> Rectangle {
         Rectangle {
-            width: width,
-            height: height
+            width,
+            height,
         }
     }
 }
 
-fn main() {}
+fn main() {
+    let mut rect = Rectangle::new(1.2, 3.4);
+    assert_eq!(rect.get_area(), 4.08);
+
+    rect.scale(0.5);
+
+    assert_eq!(rect.get_area(), 1.02);
+    println!("Test passes!");
+}
