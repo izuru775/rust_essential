67132ad93e3a78631c7ee1cb512d03771f96ab1e
izuru775 <izuru775@gmail.com>
2022-02-28 11:39:39 +1100

    Struct update syntax

--- src/main.rs
+++ src/main.rs
@@ -9,26 +9,32 @@
     functions : snake_case
     Methods : snake_case
 */
+#[derive(Debug)]
+#[derive(Clone)]
 
-use std::fs;
-use std::env;
+
+struct Shuttle{
+    name:String,
+    crew_size:u8,
+    propellant:f64
+}
 
 fn main() {
 
-    if env::args().len() <= 2 {
-        eprintln!("Program Require two arguments: <file Path> <search name>");
-        std::process::exit(1);
-    }
-    let  file_name = env::args().nth(1).unwrap();
-    let  walker_name = env::args().nth(2).unwrap();
+    let mut vehicle = Shuttle{
+        name:String::from("Endeavour"),
+        crew_size:8,
+        propellant:835958.0
+    };
 
-    for lines in fs::read_to_string(file_name).unwrap().lines(){
-        if lines == walker_name{
-            println!("{} did walk on the Moon!",walker_name);
-            return;
-        }
-    }
+    let mut vehicle2 = Shuttle{
+        ..vehicle.clone()
+    };
+    
+    vehicle.crew_size = 6;
+    
 
-    println!("{} did NOT walk on the Moon....YET!",walker_name);
-       
+    println!("vehicle is {:?}",vehicle);
+    println!("vehicle2 is {:?}",vehicle2);
+
 }
