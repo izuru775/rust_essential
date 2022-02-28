55da3ef8ae1de0671cbbe92b6a93f1db6a44a429
izuru775 <izuru775@gmail.com>
2022-02-28 13:49:15 +1100

    Half done

--- src/main.rs
+++ src/main.rs
@@ -10,21 +10,16 @@
     Methods : snake_case
 */
 
-struct Color(u8,u8,u8); // RGB
-struct Point(u8,u8,u8); // XYZ
-
-fn get_y(p:Point) -> u8{
-    p.1
+struct Rectangle{
+    width:f64,
+    height:f64
+}
+impl fn get_area(){
+    
 }
 
 fn main() {
 
- let red = Color(255,0,0);
- println!("First value is {}",red.0);
  
- let coord = Point(4,5,6);
-
- let y = get_y(coord);
- println!("value of y is {}",y);
 
 }
