aa9493962fe382ca9599966b08c91dd96a676ac9
izuru775 <izuru775@gmail.com>
2022-02-28 13:24:14 +1100

    Struct method completed

--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,7 @@
     functions : snake_case
     Methods : snake_case
 */
-#[derive(Debug)]
-#[derive(Clone)]
+
 
 
 struct Shuttle{
@@ -19,22 +18,29 @@ struct Shuttle{
     propellant:f64
 }
 
+impl Shuttle{
+    fn get_name(&self)->&str{
+        &self.name
+    }
+
+    fn add_fuel(&mut self,gallons:f64){
+        self.propellant+=gallons;
+    }
+}
+
 fn main() {
 
     let mut vehicle = Shuttle{
         name:String::from("Endeavour"),
         crew_size:8,
-        propellant:835958.0
+        propellant:0.0
     };
 
-    let mut vehicle2 = Shuttle{
-        ..vehicle.clone()
-    };
-    
-    vehicle.crew_size = 6;
-    
+    let hehicle_name = vehicle.get_name();
+    println!("vehicle name is {}",hehicle_name);
 
-    println!("vehicle is {:?}",vehicle);
-    println!("vehicle2 is {:?}",vehicle2);
+    println!("propellent is {}",vehicle.propellant);
+    vehicle.add_fuel(1000.0);
+    println!("propellent is {}",vehicle.propellant);
 
 }
