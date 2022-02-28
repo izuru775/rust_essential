eae82ea2ee629fbc86eab33582387d8df32a47fd
izuru775 <izuru775@gmail.com>
2022-02-28 16:47:29 +1100

    Boxed structure completed

--- src/main.rs
+++ src/main.rs
@@ -9,14 +9,28 @@
     functions : snake_case
     Methods : snake_case
 */
-fn get_biggest<T:PartialOrd>(a: T, b: T) -> T {
-    if a > b {
-        a
-    } else {
-        b
-    }
+use std::mem;
+
+struct Shuttle{
+    name:String,
+    crew_size:u8,
+    propellant:f64
 }
 
 fn main() {
-    println!("biggest is {}", get_biggest(1.3, 2.4));
+    let vehicle =Shuttle{
+        name:String::from("Atlantis"),
+        crew_size:7,
+        propellant:835958.0
+    };
+
+    println!("vehicle size on stack : {} bytes",mem:: size_of_val(&vehicle));
+
+    let boxed_vehicle:Box<Shuttle> = Box::new(vehicle);
+    println!("boxed_vehicle size on stack : {} bytes",mem:: size_of_val(&boxed_vehicle));
+    println!("boxed_vehicle size on heap : {} bytes",mem:: size_of_val(&*boxed_vehicle));
+
+    let unboxed_vehicle:Shuttle = *boxed_vehicle;
+    println!("unboxed_vehicle size on stack : {} bytes",mem:: size_of_val(&unboxed_vehicle));
+
 }
