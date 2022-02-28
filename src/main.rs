39a70c5f059bd965fac9f17cd0fc8d870e508d08
izuru775 <izuru775@gmail.com>
2022-02-28 16:29:34 +1100

    Generic function definition completed

--- src/main.rs
+++ src/main.rs
@@ -9,31 +9,14 @@
     functions : snake_case
     Methods : snake_case
 */
-#[derive(Debug)]
-struct Rectangle<T, U> {
-    width: T,
-    height: U,
-}
-
-impl<T, U> Rectangle<T, U> {
-    fn get_width(&self) -> &T {
-        &self.width
-    }
-}
-
-impl Rectangle<u8, u8> {
-    fn get_perimeter(&self) -> u8 {
-        2 * self.width + 2 * self.height
+fn get_biggest<T:PartialOrd>(a: T, b: T) -> T {
+    if a > b {
+        a
+    } else {
+        b
     }
 }
 
 fn main() {
-    let rect = Rectangle {
-        width: 1u8,
-        height: 3u8,
-    };
-    println!("rect is {:?}", rect);
-    println!("width is {:?}", rect.get_width());
-    println!("perimeter is {:?}", rect.get_perimeter());
-
+    println!("biggest is {}", get_biggest(1.3, 2.4));
 }
