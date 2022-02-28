446a6394453a559d14e456257f1eb0a8e3559afd
izuru775 <izuru775@gmail.com>
2022-02-28 13:42:46 +1100

    Tuple structs completed

--- src/main.rs
+++ src/main.rs
@@ -10,43 +10,21 @@
     Methods : snake_case
 */
 
+struct Color(u8,u8,u8); // RGB
+struct Point(u8,u8,u8); // XYZ
 
-
-struct Shuttle{
-    name:String,
-    crew_size:u8,
-    propellant:f64
-}
-
-impl Shuttle{
-    fn get_name(&self)->&str{
-        &self.name
-    }
-
-    fn add_fuel(&mut self,gallons:f64){
-        self.propellant+=gallons;
-    }
-
-    fn new(name:&str)->Shuttle{
-        Shuttle{
-            name:String::from(name),
-            crew_size:7,
-            propellant:0.0
-        }
-    }
+fn get_y(p:Point) -> u8{
+    p.1
 }
 
 fn main() {
 
-    let mut vehicle = Shuttle::new("Endeavour");
-    let mut vehicle2 = Shuttle::new("Discovery");
+ let red = Color(255,0,0);
+ println!("First value is {}",red.0);
+ 
+ let coord = Point(4,5,6);
 
-
-    let hehicle_name = vehicle.get_name();
-    println!("vehicle name is {}",hehicle_name);
-
-    println!("propellent is {}",vehicle.propellant);
-    vehicle.add_fuel(1000.0);
-    println!("propellent is {}",vehicle.propellant);
+ let y = get_y(coord);
+ println!("value of y is {}",y);
 
 }
