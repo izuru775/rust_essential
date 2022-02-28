d591d3361b2713aa5dd506b3c5becd4aec3aea1b
izuru775 <izuru775@gmail.com>
2022-02-28 15:32:32 +1100

    Generic definition completed

--- src/main.rs
+++ src/main.rs
@@ -9,34 +9,17 @@
     functions : snake_case
     Methods : snake_case
 */
-
-struct Rectangle {
-    width: f64,
-    height: f64,
+#[derive(Debug)]
+struct Rectangle<T,U> {
+    width: T,
+    height: U,
 }
-impl Rectangle {
-    fn get_area(&self) -> f64 {
-        self.width * self.height
-    }
 
-    fn scale(&mut self, scale: f64) {
-        self.width *= scale;
-        self.height *= scale;
-    }
-    fn new(width: f64, height: f64) -> Rectangle {
-        Rectangle {
-            width,
-            height,
-        }
-    }
-}
 
 fn main() {
-    let mut rect = Rectangle::new(1.2, 3.4);
-    assert_eq!(rect.get_area(), 4.08);
-
-    rect.scale(0.5);
-
-    assert_eq!(rect.get_area(), 1.02);
-    println!("Test passes!");
+    let rect =Rectangle{
+        width:1u8,
+        height:3u16
+    };
+    println!("rect is {:?}",rect);
 }
