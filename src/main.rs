b70eeb5861850780a5b75566fc3707acd87d76c0
izuru775 <izuru775@gmail.com>
2022-03-01 19:00:32 +1100

    Derive traites completed

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,22 @@
+#[derive(PartialEq,PartialOrd)]
 struct Satellite{
     name:String,
     velocity:f64 // miles per second
 }
-
-struct SpaceStation{
-    name:String,
-    crew_size:u8,
-    altitude:u32 // miles
-}
-// Trait structure
-trait Description{
-    fn describe(&self)->String{
-        String::from("an object flying through space!")
-    }
-}
-
-// Implementing trait to Satellite struct
-impl Description for Satellite{
-    // fn describe(&self)->String{
-    //     format!("The {} is flying at {} miles per second",self.name,self.velocity)
-    // }
-}
-
-// Implementing trait to SpaceStation struct
-impl Description for SpaceStation{
-    fn describe(&self)->String{
-        format!("The {} is flying {} miles hight with {} crew members on board!",self.name,self.altitude,self.crew_size)
-    }
-}
-
 
 fn main() {
     let hubble = Satellite{
         name:String::from("Hubble Telescope"),
         velocity:4.72
     };
-
-    let iss = SpaceStation{
-        name:String::from("International Space Station"),
-        crew_size:6,
-        altitude:254
+    let gps = Satellite{
+        name:String::from("GPS"),
+        velocity:2.42
     };
+  
 
-    println!("hubble is : {}",hubble.describe());
-    println!("iss is : {}",iss.describe());
-    
+    println!("hubble == gps is {}",hubble== gps);
+    println!("hubble > gps is {}",hubble > gps);
+
+  
 }
