33f3afbab9b3e3b8347e4da67c4bf6cd9180d4dd
izuru775 <izuru775@gmail.com>
2022-03-04 02:05:40 +1100

    I did it myself!

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,23 @@
-fn main() {
-    let number = Some(13);
-
-    // match number{
-    //     Some(13)=>println!("Thirteen"),
-    //     _=> ()
-
-    // }
-    
-
-    if let Some(13) = number {
-        println!("thirteen");
+enum Location {
+    Unkown,
+    Anonymous,
+    Known(f64,f64)
+}
+impl Location{
+    fn display(&self){
+        match *self{
+            Location::Anonymous=>println!("location is anonymous"),
+            Location::Unkown =>println!("location is known"),
+            Location::Known(lat,lon) => println!("latitude is {} and longitude is {}",lat,lon)
+        }
     }
-
+}
+fn main() {
+    let address = Location::Anonymous;
+    address.display();
+    let address = Location::Unkown;
+    address.display();
+    let address = Location::Known(28.608295,-80.604177);
+    address.display();
 
 }
