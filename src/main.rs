27c22783f6dc45c6ab3d6e57e865520a13d996bf
izuru775 <izuru775@gmail.com>
2022-03-03 16:32:27 +1100

    Challenge completed

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,22 @@
 use std::fmt;
 
-fn get_displayable(choice:bool)->impl fmt::Display{
-    if choice {
-        13
-    }else{
-        "thirteen"
+
+struct Satellite {
+    name: String,
+    velocity: f64, // miles per second
+}
+
+impl fmt::Display for Satellite {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{} flying at {} miles per hour", self.name, self.velocity)
     }
 }
 
-fn main() {
-    println!("output is {}", get_displayable(true));
+
+fn main(){
+    let hubble = Satellite{
+        name: String::from("Hubble Telescope"),
+        velocity: 4.72
+    };
+    println!("hubble is {}",hubble);
 }
