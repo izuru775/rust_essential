a3d9e8e9d6755ac337f4c1be43b5639156044c77
izuru775 <izuru775@gmail.com>
2022-03-04 00:55:06 +1100

    Completed

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,23 @@
+#[derive(Debug)]
+enum Shapes{
+    Circle(f64), // radius
+    Rectangle(f64,f64), // width, height
+    Triangle(f64,f64,f64) // sides a,b,c
+}
+
+impl Shapes{
+    fn get_perimeter(&self)->f64{
+        match *self{
+            Shapes::Circle(r)=> r * 2.0 * std::f64::consts::PI,
+            Shapes::Rectangle(w,h) =>(2.0 * w) + (2.0 * h),
+            Shapes::Triangle(a,b,c) => a+b+c
+        }
+    }
+}
 fn main() {
-    let my_number = 10u8;
+    let my_shape = Shapes::Rectangle(1.2,3.4);
+    println!("my_shape is {:?}",my_shape);
 
-    let result = match my_number{
-        0 =>"zero",
-        1 =>"one",
-        2 =>"two",
-        3 =>"three",
-        _ =>{
-            println!("{} did not match",my_number);
-            "something else"
-        } 
-    };
-    println!("result is {}", result);
-
+    let perimeter = my_shape.get_perimeter();
+    println!("perimeter is {}",perimeter);
 }
