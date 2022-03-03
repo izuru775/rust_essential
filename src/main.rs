a0e4d09a4e55c2b7cd4a7f89a7eb9c588104dce2
izuru775 <izuru775@gmail.com>
2022-03-03 23:48:57 +1100

    Completed

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,18 @@
-fn best_fuel<'a,'b>(x: &'a str, y: &'b str) -> &'a str {
-    if x.len() > y.len() {
-        x
-    } else {
-        x
+struct Shuttle<'a>{
+    name:&'a str
+}
+impl<'a,'b> Shuttle<'a> {
+    fn send_transmission(&'a self,msg:&'b str)->&'b str{
+        println!("Transmitting message: {}",msg);
+        msg
     }
 }
 
 fn main() {
-    let result;
-    let propellent1 = String::from("RP-1");
-    {
-        let propellent2 = String::from("LNG");
-        result = best_fuel(&propellent1, &propellent2);
-    }
-    println!("result is {}", result);
+    let vehicle = Shuttle{
+        name:"Endeavor"
+    };
+
+    let sender = vehicle.send_transmission("Greetings from orbit!");
+    println!("sender is {}",sender);
 }
