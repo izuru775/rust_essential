b6ef1a8cdb44896416bd2e23daae4dfd4c2cb505
izuru775 <izuru775@gmail.com>
2022-03-04 01:30:50 +1100

    If let completed

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,16 @@
 fn main() {
-    let countdown = [5,4,3,2,1];
-    let number = countdown.get(5); // get() method returns a "option enum" holding a refernce to a value at the specified enum
-    let number = number.unwrap_or(&0) +1;
-    println!("Number is {:?}",number);
+    let number = Some(13);
+
+    // match number{
+    //     Some(13)=>println!("Thirteen"),
+    //     _=> ()
+
+    // }
+    
+
+    if let Some(13) = number {
+        println!("thirteen");
+    }
+
+
 }
