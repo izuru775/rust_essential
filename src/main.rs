2e9c776e1ce22baad1a418cffbac9e847b7a35c1
izuru775 <izuru775@gmail.com>
2022-03-04 01:20:11 +1100

    Match option completed

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 fn main() {
     let countdown = [5,4,3,2,1];
-    let number = countdown.get(5); // get() method returns a "option enum" holding a refernce to a value at the specified enum
-    let number = number.unwrap_or(&0) +1;
+    let number = countdown.get(2); // get() method returns a "option enum" holding a refernce to a value at the specified enum
+    let number = match number{
+        Some(number)=>number+1,
+        None => 0
+    };
     println!("Number is {:?}",number);
 }
