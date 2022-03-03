35576882159c10a4ad237be778c09a4fae1f872b
izuru775 <izuru775@gmail.com>
2022-03-04 00:27:29 +1100

    Completed

--- src/main.rs
+++ src/main.rs
@@ -9,4 +9,10 @@ fn main() {
     let my_shape = Shape::Rectangle(1.2,3.4);
     println!("my_shape is {:?}",my_shape);
 
+    match my_shape{
+        Shape::Circle(r)=>println!("Circle with a radius {}",r),
+        Shape::Rectangle(w,h)=>println!("{} x {} Rectangle",w,h),
+        Shape::Triangle(a,b,c)=>println!("Triangle with sides {},{},{}",a,b,c)
+    }
+
 }
