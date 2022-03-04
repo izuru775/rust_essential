14d8212466e696bf39522aa1ff834b76af66d4b2
izuru775 <izuru775@gmail.com>
2022-03-04 22:05:10 +1100

    Propagating errors completed

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,22 @@
 use std::fs;
 use std::io;
 
+fn read_and_combine(f1:&str,f2:&str)->Result<String,io::Error>{
+    let mut s1 = fs::read_to_string(f1)?;
+    let s2 = match fs::read_to_string(f2){
+        Ok(s) =>s,
+        Err(e)=> return Err(e)
+    };
+    s1.push('\n');
+    s1.push_str(&s2);
+    Ok(s1)
+
+}
+
 fn main() {
-    let result = fs::read_to_string("the_ultimate_question.txt");
-    let content = match result{
-        Ok(message) => message,
-        Err(error) => match error.kind(){
-            io::ErrorKind::NotFound => String::from("File not found."),
-            io::ErrorKind::PermissionDenied => String::from("Permission denied"),
-            _=> panic!("Another type of error: {:?}",error)
-        }
-    }; 
-    println!("content is: {:?}",content);
+    let result = read_and_combine("planet.txt", "dwarf_planets.txt");
+    match result{
+        Ok(s)=>println!("result is ...\n{}",s),
+        Err(e)=>println!("there was an error: {}",e)
+    }
 }
